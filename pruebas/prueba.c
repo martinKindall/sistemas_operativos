0b3740d0e408b269d5a6ed66142aca706eec3850
martinKindall <martin@rflex.cl>
2018-09-20 11:08:16 -0300

    Primer ejemplo Aux1

---
+++ pruebas/prueba.c
@@ -0,0 +1,28 @@
+#include "nSystem.h"
+
+
+int escritor(int num, int espera);
+
+
+int nMain()
+{
+	nTask tareas[3];
+	int i;
+	for (i = 0; i < 3; ++i)
+	tareas[i] = nEmitTask(escritor, i, i * 200);
+	for (i = 0; i < 3; ++i)
+	nWaitTask(tareas[i]);
+	nPrintf("Fin ejemplo\n");
+}
+
+
+int escritor(int num, int espera) 
+{
+	int i = 5;
+	while (i > 0) 
+	{
+		nPrintf("thread %d: %d\n", num, i);
+		nSleep(espera);
+		i--;
+	}
+}
